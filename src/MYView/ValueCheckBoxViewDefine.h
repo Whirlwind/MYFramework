b2f25ccf4c7d27314109c271485bfc891f871fed
alex <zhangzhichao@boohee.com>
2012-12-14 13:08:36 +0800

    [UI] 修正颜色的值

--- src/MYView/ValueCheckBoxViewDefine.h
+++ src/MYView/ValueCheckBoxViewDefine.h
@@ -16,16 +16,16 @@
 #define kValueCheckBoxViewPointerImage @"pointer"
 
 // 显示的数值（顶部中间）
-#define kValueCheckBoxViewValueSelectedLabelTextColor [UIColor colorWithRed:229.0/255.0 green:20/255.0f blue:0 alpha:1]
+#define kValueCheckBoxViewValueSelectedLabelTextColor [UIColor colorWithRed:229.0/255.0f green:20/255.0f blue:0.0f alpha:1]
 #define kValueCheckBoxViewValueSelectedLabelTextFont [UIFont fontWithName:@"Helvetica Neue" size:36]
 
 // 显示的单位（顶部右侧）
-#define kValueCheckBoxViewUnitSelectedLabelTextColor [UIColor colorWithRed:51/255 green:51/255 blue:51/255 alpha:1]
+#define kValueCheckBoxViewUnitSelectedLabelTextColor [UIColor colorWithRed:51/255.0f green:51/255.0f blue:51/255.0f alpha:1]
 #define kValueCheckBoxViewUnitSelectedLabelTextFont [UIFont systemFontOfSize:18]
 
 // 单位换算 (顶部左侧)
-#define kValueCheckBoxViewSubValueSelectedLabelTextColor [UIColor colorWithRed:51/255 green:51/255 blue:51/255 alpha:1]
+#define kValueCheckBoxViewSubValueSelectedLabelTextColor [UIColor colorWithRed:51/255.0f green:51/255.0f blue:51/255.0f alpha:1]
 
 // 个性化单位 （底部）
-#define kValueCheckBoxViewUnitTitleColorForNormal [UIColor colorWithRed:153/255 green:153/255 blue:153/255 alpha:1]
-#define kValueCheckBoxViewUnitTitleColorForSelected [UIColor colorWithRed:229.0/255.0 green:20/255.0f blue:0 alpha:1]
+#define kValueCheckBoxViewUnitTitleColorForNormal [UIColor colorWithRed:153/255.0f green:153/255.0f blue:153/255.0f alpha:1]
+#define kValueCheckBoxViewUnitTitleColorForSelected [UIColor colorWithRed:229.0/255.0f green:20/255.0f blue:0.0f alpha:1]
