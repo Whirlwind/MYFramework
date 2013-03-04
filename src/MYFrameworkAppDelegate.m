c1bf76ce7a196f1f12a6d148c8128097551bcc7b
Whirlwind <james@boohee.com>
2013-03-04 21:13:15 +0800

    [add]add MYFrameworkAppDelegate

---
+++ src/MYFrameworkAppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  MYFrameworkAppDelegate.m
+//  MYFrameworkDemo
+//
+//  Created by Whirlwind on 13-3-4.
+//  Copyright (c) 2013年 BOOHEE. All rights reserved.
+//
+
+#import "MYFrameworkAppDelegate.h"
+
+@implementation MYFrameworkAppDelegate
+
+- (void)dealloc {
+    [_window release], _window = nil;
+    [super dealloc];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [MYBroadcastCenter addFileInBundle];
+    [MYRouteCenter addFileInBundle];
+    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+    POST_BROADCAST_WITH_ARGS(@{@"window" : self.window});
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
