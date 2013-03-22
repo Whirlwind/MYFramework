5e9d21e1266791693b678ce1034c0f00fd6e14cb
Whirlwind <james@boohee.com>
2013-03-22 16:53:19 +0800

    [add]add MYOrderedMutableDictionary

---
+++ src/MYFoundation/MYOrderedMutableDictionary.h
@@ -0,0 +1,25 @@
+//
+//  MYOrderedMutableDictionary.h
+//  foodiet
+//
+//  Created by 詹 迟晶 on 11-12-29.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYOrderedMutableDictionary : NSMutableDictionary
+@property (nonatomic, retain) NSMutableArray *keyOrder;
+@property (nonatomic, retain) NSMutableDictionary *dic;
+
+- (id)initWithCapacity:(NSUInteger)numItems;
+- (int)indexOfString:(NSString *)key;
+- (void)removeObjectForString:(NSString *)key;
+- (void)removeAllObjects;
+- (id)objectForString:(NSString *)key;
+- (id)objectForKey:(id)key;
+- (id)objectAtIndex:(NSUInteger)index;
+- (id)lastObject;
+- (id)firstObject;
+- (NSUInteger)count;
+@end
