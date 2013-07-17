c2538880fa91e5f5a601a831832b5728e53e0e7f
Whirlwind <whirlwindjames@foxmail.com>
2013-07-17 18:50:30 +0800

    [add]add placeholder of MYTextView

--- src/MYFoundation/MYTextView.h
+++ src/MYFoundation/MYTextView.h
@@ -9,10 +9,23 @@
 #import <UIKit/UIKit.h>
 
 @interface MYTextView : UITextView
+/**
+ The string that is displayed when there is no other text in the text view.
+
+ The default value is `nil`.
+ */
+@property (nonatomic, strong) NSString *placeholder;
+
+/**
+ The color of the placeholder.
+
+ The default is `[UIColor lightGrayColor]`.
+ */
+@property (nonatomic, strong) UIColor *placeholderTextColor;
 
 @property (assign, nonatomic) BOOL allowSelection;
 @property (strong, nonatomic) NSAttributedString *attributedString;
-
+@property (nonatomic, assign) BOOL isEditing;
 - (CGFloat)lineHeight;
 
 @end
--- src/MYFoundation/MYTextView.m
+++ src/MYFoundation/MYTextView.m
@@ -17,24 +17,25 @@
 
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        self.allowSelection = YES;
+        self.contentInset = UIEdgeInsetsMake(8.f, self.contentInset.left, self.contentInset.bottom, self.contentInset.right);
+        [self _initialize];
     }
     return self;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
     if (self = [super initWithCoder:aDecoder]) {
-        self.allowSelection = YES;
+		[self _initialize];
     }
     return self;
 }
 
 - (CGFloat)lineHeight {
-    return 2.f;
+    return 20.f;
 }
 
 - (id)styleString {
-    return [[super styleString] stringByAppendingString:[NSString stringWithFormat:@"; line-height: %fem", [self lineHeight]]];
+    return [[super styleString] stringByAppendingString:[NSString stringWithFormat:@"; line-height: %fpx", [self lineHeight]]];
 }
 
 - (void)setAttributedString:(NSAttributedString *)attributedString {
@@ -46,6 +47,124 @@
 }
 
 - (BOOL)canBecomeFirstResponder {
-    return self.allowSelection;
+    if (self.allowSelection) {
+        return [super canBecomeFirstResponder];
+    }
+    return NO;
+}
+
+- (void)setText:(NSString *)string {
+	[super setText:string];
+	[self setNeedsDisplay];
+}
+
+
+- (void)insertText:(NSString *)string {
+	[super insertText:string];
+	[self setNeedsDisplay];
+}
+
+
+- (void)setAttributedText:(NSAttributedString *)attributedText {
+	[super setAttributedText:attributedText];
+	[self setNeedsDisplay];
+}
+
+
+- (void)setPlaceholder:(NSString *)string {
+	if ([string isEqual:_placeholder]) {
+		return;
+	}
+
+	_placeholder = string;
+	[self setNeedsDisplay];
+}
+
+
+- (void)setContentInset:(UIEdgeInsets)contentInset {
+	[super setContentInset:contentInset];
+	[self setNeedsDisplay];
+}
+
+
+- (void)setFont:(UIFont *)font {
+	[super setFont:font];
+	[self setNeedsDisplay];
+}
+
+
+- (void)setTextAlignment:(NSTextAlignment)textAlignment {
+	[super setTextAlignment:textAlignment];
+	[self setNeedsDisplay];
+}
+
+
+#pragma mark - NSObject
+
+- (void)dealloc {
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:UITextViewTextDidChangeNotification object:self];
+}
+
+
+#pragma mark - UIView
+
+- (void)drawRect:(CGRect)rect {
+	[super drawRect:rect];
+
+	if (self.text.length == 0 && self.placeholder) {
+		// Inset the rect
+		rect = UIEdgeInsetsInsetRect(rect, self.contentInset);
+
+		// TODO: This is hacky. Not sure why 8 is the magic number
+		if (self.contentInset.left == 0.0f) {
+			rect.origin.x += 8.0f;
+		}
+        if (self.contentInset.top == 0.0f) {
+            rect.origin.y += 8.0f;
+        }
+        rect.origin.y += 3.f;
+		// Draw the text
+		[_placeholderTextColor set];
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_6_0
+		[_placeholder drawInRect:rect withFont:self.font lineBreakMode:NSLineBreakByTruncatingTail alignment:self.textAlignment];
+#else
+		[_placeholder drawInRect:rect withFont:self.font lineBreakMode:UILineBreakModeTailTruncation alignment:self.textAlignment];
+#endif
+	}
+}
+
+
+#pragma mark - Private
+
+- (void)_initialize {
+    self.allowSelection = YES;
+    self.isEditing = NO;
+	self.placeholderTextColor = [UIColor colorWithWhite:0.702f alpha:1.0f];
+
+	[[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(_textBeginEditing:)
+                                                 name:UITextViewTextDidBeginEditingNotification
+                                               object:self];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(_textChanged:)
+                                                 name:UITextViewTextDidChangeNotification
+                                               object:self];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(_textEndEditing:)
+                                                 name:UITextViewTextDidEndEditingNotification
+                                               object:self];
+}
+
+
+- (void)_textChanged:(NSNotification *)notification {
+	[self setNeedsDisplay];
+}
+
+- (void)_textBeginEditing:(NSNotification *)notification {
+	[self setIsEditing:YES];
+}
+
+- (void)_textEndEditing:(NSNotification *)notification {
+	[self setIsEditing:NO];
 }
 @end
