19c1486313ff9c0b85de9d89f04ab5f1c107c97e
Whirlwind <whirlwindjames@foxmail.com>
2013-08-07 22:21:56 +0800

    [add]add -[NSNull stringValue]

---
+++ src/MYFoundation/NSNull+stringValue.h
@@ -0,0 +1,13 @@
+//
+//  NSNull+stringValue.h
+//  Pods
+//
+//  Created by Whirlwind on 13-8-7.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNull (stringValue)
+
+@end
---
+++ src/MYFoundation/NSNull+stringValue.m
@@ -0,0 +1,13 @@
+//
+//  NSNull+stringValue.m
+//  Pods
+//
+//  Created by Whirlwind on 13-8-7.
+//
+//
+
+#import "NSNull+stringValue.h"
+
+@implementation NSNull (stringValue)
+
+@end
